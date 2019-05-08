9defeb5de52a2f1c220b369660a6c131494974fd
Islam Bayoumy <islam.bayoumy@gmail.com>
2019-05-08 12:19:23 +0200

    Add: "add node to tail" method with unit test

--- Doubly LinkedList/DoublyLinkedList.py
+++ Doubly LinkedList/DoublyLinkedList.py
@@ -29,6 +29,32 @@ class DoublyLinkedList:
     def __init__(self):
         self.head = None
 
+    def add_tail(self, data):
+        """
+        Insert new Node at the ending of the Doubly LinkedList
+        """
+        node = DoublyLinkedListNode(data)
+        if not self.head:
+            node.previous = None
+            self.head = node
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+
+            current.next = node
+            node.previous = current
+            node.next = None
+
+    def get_tail(self):
+        """
+        Return tail of LinkedList
+        """
+        current = self.head
+        while current.next:
+            current = current.next
+        return current
+
     def print_list(self):
         """
         Print the whole LinkedList
@@ -45,6 +71,10 @@ Method just for testing
 """
 def test_func():
     dllist = DoublyLinkedList()
+
+    dllist.add_tail(1)
+    dllist.add_tail(2)
+    dllist.add_tail(3)
 
     dllist.print_list()
 
--- Doubly LinkedList/test_DoublyLinkedList.py
+++ Doubly LinkedList/test_DoublyLinkedList.py
@@ -6,8 +6,15 @@ from DoublyLinkedList import DoublyLinkedList
 def dllist(request):
     """Creating a DoublyLinkedList object"""
     dllist = DoublyLinkedList()
+    dllist.add_tail(5)
+    dllist.add_tail(6)
+    dllist.add_tail(7)
     return dllist
 
 # just for coverage
 def test_print_list(dllist):
     assert not dllist.print_list()
+
+def test_add_to_tail(dllist):
+    dllist.add_tail(100)
+    assert 100 == dllist.get_tail().data, "not a tail"
