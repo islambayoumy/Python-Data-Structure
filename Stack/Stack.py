e0a75005e4e889fef07597b641376114969b945e
Islam Bayoumy <islam.bayoumy@gmail.com>
2019-05-14 11:30:54 +0200

    Add: pop method

--- Stack/Stack.py
+++ Stack/Stack.py
@@ -5,9 +5,17 @@ class Stack:
     def __init__(self):
         self.items = list()
 
+
     def push(self, item):
         """
         Pushing new item to the top of the stack
         """
         self.items.append(item)
         return
+
+
+    def pop(self):
+        """
+        pop the top item of the stack
+        """
+        return self.items.pop()
