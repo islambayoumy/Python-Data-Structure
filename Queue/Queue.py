9e7f2dabfb41fd456bb0916167d070c5b2c650a9
Islam Bayoumy <islam.bayoumy@gmail.com>
2019-05-20 11:26:12 +0200

    Make: Queue DS skeleton

---
+++ Queue/Queue.py
@@ -0,0 +1,66 @@
+# Queue Data Structure [FIFO]
+
+
+class Queue:
+    def __init__(self):
+        self.items = list()
+
+    def enqueue(self, item):
+        """
+        Add new item to the end of the queue
+        """
+        pass
+
+    def dequeue(self):
+        """
+        Return and remove first item from the queue
+        """
+        pass
+
+    def is_empty(self):
+        """
+        Checking if the queue is empty?
+        """
+        pass
+
+    def get_max(self):
+        """
+        Return max in the queue
+        """
+        pass
+
+    def queue_len(self):
+        """
+        Return length of the queue
+        """
+        pass
+
+    def get_queue(self):  # pragma: no cover
+        """
+        Return the queue
+        """
+        pass
+
+    def get_first(self):
+        """
+        Return first item added to the queue
+        """
+        pass
+
+    def get_last(self):
+        """
+        Return last item added to the queue
+        """
+        pass
+
+
+"""
+Method just for testing
+"""
+
+
+def test_func():  # pragma: no cover
+    pass
+
+
+# test_func()
